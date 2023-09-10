2213041f4b6ec0bb2f42d5f85cdb786d954ce080
manou12345 <66104950+manou12345@users.noreply.github.com>
2023-09-10 18:44:29 +0000

    changed design and form

--- app.py
+++ app.py
@@ -2,24 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-CLASSES = [
-  {
-    'id': 1,
-    'name': 'Intermediate Algebra'
-  },
-    {
-    'id': 2,
-    'name': 'College Algebra'
-  },
-    {
-    'id': 3,
-    'name': 'Calculus 1'
-  }
-]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',
-                        classs = CLASSES)
+  return render_template('home.html')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
